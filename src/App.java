4744afad11a01ae649dcee286998fe3b097e6b08
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-09-25 09:14:40 +0200

    Gjort klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //a
+        int[] fält1 = new int[5];
+        fält1[0] = 8;
+        fält1[1] = 4;
+        fält1[2] = 6;
+        fält1[3] = 3;
+        fält1[4] = 9;
+        int antal = fält1.length;
+        int summa = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
+        double medelvärde = summa/5;
+        System.out.println("Fält1 inehåller "+fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4]+ " antal element är "+antal+ " och medelvvärdet är "+medelvärde);
     }
 }
