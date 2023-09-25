ddfcfc88b7ea776d73aef6b7f6bd12c8f2a8b6dd
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-09-25 09:25:03 +0200

    Gjort kalr del b

--- src/App.java
+++ src/App.java
@@ -11,6 +11,18 @@ public class App {
         int antal = fält1.length;
         int summa = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
         double medelvärde = summa/5;
-        System.out.println("Fält1 inehåller "+fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4]+ " antal element är "+antal+ " och medelvvärdet är "+medelvärde);
+        System.out.println("Fält1 inehåller "+fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4]+ " antal element är "+antal+ " och medelvärdet för fält 1 är "+medelvärde);
+
+        //b
+        int[] fält2 = new int[5];
+        fält2[0] = 18;
+        fält2[1] = 14;
+        fält2[2] = 16;
+        fält2[3] = 13;
+        fält2[4] = 19;
+        int summaf12 = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4]+fält2[0]+fält2[1]+fält2[2]+fält2[3]+fält2[4];
+        int summa1 = fält2[0]+fält2[1]+fält2[2]+fält2[3]+fält2[4];
+        double medelvärde1 = summa1/5;
+        System.out.println("Summan av fält1 och fält2 är "+summaf12+" och medelvärdet för fält 2 är "+medelvärde1);
     }
 }
